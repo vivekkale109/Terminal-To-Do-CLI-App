0c8d605f8e282e37869eb41c23e4282df4fa55ab
vivek <vivekkale109@gmail.com>
2026-03-23 18:28:36 +0530

    add the ansi colors

--- To-Do.c
+++ To-Do.c
@@ -5,3 +5,9 @@
 #define FILE_NAME "tasks.json"
 #define MAX_TASKS 100
 #define MAX_TITLE 256
+
+// ANSI colors
+#define GREEN "\033[0;32m"
+#define RED "\033[0;31m"
+#define YELLOW "\033[0;33m"
+#define RESET "\033[0m"
